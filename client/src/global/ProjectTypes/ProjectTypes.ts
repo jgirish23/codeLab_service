1ee501ce95a2ca28118fda007c0d95e48b1abbdd
jgirish23 <jakkalagirish@gmail.com>
2025-12-24 16:58:41 +0530

    Removed client code and added modified cors config

--- client/src/global/ProjectTypes/ProjectTypes.ts
+++
@@ -1,6 +0,0 @@
-export const ProjectTypes = [
-    { value: '0', text: 'Select Project Name' },
-    { value: '1', text: 'React js' },
-    { value: '2', text: 'Python' },
-    { value: '3', text: 'javascript' }
-  ];
